bfa8e91fdec5427c5e3a57adc84a2563cb3c88b5
origin_ex <f_origin@163.com>
2018-12-22 19:05:09 +0800

    update

--- models-r1.5/research/run.sh
+++ models-r1.5/research/run.sh
@@ -10,11 +10,14 @@ export PYTHONPATH=$PYTHONPATH:$DIR:$DIR/slim:$DIR/object_detection
 
 # 定义各目录
 output_dir=/output  # 训练目录
+mkdir $output_dir
 dataset_dir=/data/forigin/week9-data # 数据集目录，这里是写死的，记得修改
 
 train_dir=$output_dir/train
+mkdir $train_dir
 checkpoint_dir=$train_dir
 eval_dir=$output_dir/eval
+mkdir $eval_dir
 
 # config文件??
 config=ssd_mobilenet_v1_pets.config
