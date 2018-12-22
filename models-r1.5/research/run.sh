a997e5cb7d4696684c8aa861162d9018faf2bd11
origin_ex <f_origin@163.com>
2018-12-22 17:57:30 +0800

    update

--- models-r1.5/research/run.sh
+++ models-r1.5/research/run.sh
@@ -30,8 +30,8 @@ cp $dataset_dir/$config $pipeline_config_path
 for i in {0..4}  # for循环中的代码执行5此，这里的左右边界都包含，也就是一共训练500个step，每100step验证一次
 do
     echo "############" $i "runnning #################"
-    last=$[$i*20
-    current=$[($i+1)*20
+    last=$[$i*20]
+    current=$[($i+1)*20]
     sed -i "s/^  num_steps: $last$/  num_steps: $current/g" $pipeline_config_path  # 通过num_steps控制一次训练最多100step
 
     echo "############" $i "training #################"
