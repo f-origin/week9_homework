d62acc826246c9867bce01f006ddd0599d6a78e8
f-origin <f_origin@163.com>
2019-02-22 10:12:59 +0800

    Update run.sh

--- models-r1.5/research/run.sh
+++ models-r1.5/research/run.sh
@@ -34,22 +34,3 @@ echo "###dataset_dir###" $dataset_dir "###"
 # 因为dataset里面的东西是不允许修改的，所以这里要把config文件复制一份到输出目录
 cp $dataset_dir/$config $pipeline_config_path
 
-for i in {0..4}  # for循环中的代码执行5此，这里的左右边界都包含，也就是一共训练500个step，每100step验证一次
-do
-    echo "############" $i "runnning #################"
-    last=$[$i*500]
-    current=$[($i+1)*500]
-    sed -i "s/^  num_steps: $last$/  num_steps: $current/g" $pipeline_config_path  # 通过num_steps控制一次训练最多100step
-
-    echo "############" $i "training #################"
-    python3 ./object_detection/train.py --train_dir=$train_dir --pipeline_config_path=$pipeline_config_path
-
-    echo "############" $i "evaluating, this takes a long while #################"
-    python3 ./object_detection/eval.py --checkpoint_dir=$checkpoint_dir --eval_dir=$eval_dir --pipeline_config_path=$pipeline_config_path
-done
-echo "############ export model #################"
-# 导出模型
-python3 ./object_detection/export_inference_graph.py --input_type image_tensor --pipeline_config_path $pipeline_config_path --trained_checkpoint_prefix $train_dir/model.ckpt-$current  --output_directory $output_dir/exported_graphs
-echo "############ inference #################"
-# 在test.jpg上验证导出的模型
-python3 ./inference.py --output_dir=$output_dir --dataset_dir=$dataset_dir
